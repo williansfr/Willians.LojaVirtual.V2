edd69fa357b04ed9a4c78c643ba54fb6cdad904a
Willians Ferreira <williansfr@hotmail.com>
2017-05-08 08:58:57 -0300

    nn

--- Willians.LojaVirtual.UnitTest/UnitTest1.cs
+++ Willians.LojaVirtual.UnitTest/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace Willians.LojaVirtual.UnitTest
             int[] lista = { 9, 8, 7, 6, 5, 4, 3 };
 
             var numeros = from i in lista.Take(3) select i;
-
+               
             int[] itens = { 9, 8, 5 };     
 
             CollectionAssert.AreEqual(numeros.ToArray(), itens);
